b208c002ac0df3bce881cd389edbf7a4f6ab76bf
sohag-84 <injamulhaqsohag19977@gmail.com>
2022-12-12 18:59:19 +0600

    header section is completed

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Typing Script JS
+var typed = new Typed(".typing", {
+  strings: [
+    "Android Developer",
+    "Ios Developer",
+    "Flutter Developer",
+    "Java Swing Developer",
+    "Web Designer"
+  ],
+  typeSpeed: 80,
+  backSpeed: 80,
+});
