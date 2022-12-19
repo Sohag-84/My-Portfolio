bcbfc392197f9e0c0d2d44c907a3692f7e104bff
sohag-84 <injamulhaqsohag19977@gmail.com>
2022-12-19 09:43:32 +0600

    about me section done

--- js/script.js
+++ js/script.js
@@ -5,8 +5,20 @@ var typed = new Typed(".typing", {
     "Ios Developer",
     "Flutter Developer",
     "Java Swing Developer",
-    "Web Designer"
+    "Web Designer",
   ],
   typeSpeed: 80,
   backSpeed: 80,
+});
+
+var typed = new Typed(".typing-2", {
+  strings: [
+    "Android Developer",
+    "Ios Developer",
+    "Flutter Developer",
+    "Java Swing Developer",
+    "Web Designer",
+  ],
+  typeSpeed: 100,
+  backSpeed: 60,
 });
