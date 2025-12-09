dd292f55bc31882af98899cb5e5beb0cc4e04785
Sohag-84 <ihsohag84@gmail.com>
2025-12-09 13:15:22 +0600

    changes some ui desing

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 // Typing Script JS
 var typed = new Typed(".typing", {
   strings: [
-    "Android Developer",
-    "Ios Developer",
     "Flutter Developer",
+    "Android Developer",
     "Java Swing Developer",
-    "Web Designer",
+    "Flutter Developer",
   ],
   typeSpeed: 80,
   backSpeed: 80,
@@ -13,11 +12,10 @@ var typed = new Typed(".typing", {
 
 var typed = new Typed(".typing-2", {
   strings: [
-    "Android Developer",
-    "Ios Developer",
     "Flutter Developer",
+    "Android Developer",
     "Java Swing Developer",
-    "Web Designer",
+    "Flutter Developer",
   ],
   typeSpeed: 100,
   backSpeed: 60,
